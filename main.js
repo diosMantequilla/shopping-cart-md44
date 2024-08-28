f1a2d6745d333aebdce0a44f1e1940d08f7919cb
diosMantequilla <yagel.salazar.dev@gmail.com>
2024-08-28 11:48:26 -0600

    cambios hasta el jueves

--- main.js
+++ main.js
@@ -58,6 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const miNodoBoton = document.createElement('button')
             miNodoBoton.classList.add('btn', 'btn-primary')
             miNodoBoton.textContent = '+'
+            miNodoBoton.setAttribute('marcador', info.id)
+            // miNodoBoton.addEventListener('click', anadirProductosAlCarrito)
 
             // armar nuestra tarjetita de producto
             miNodoCardBody.appendChild(miNodoImagen)
@@ -68,6 +70,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
             DOMitems.appendChild(miNodo)
         })
+    }
+
+    // creamos una funcion para renderizar el carrito
+    function renderizarCarrito() {
+        // limpiar el carrito
+        DOMcarrito.textContent = ''
+        // quitamos los duplicados del carrito
+        const carritoSinDuplicados = [...new Set(carrito)]
+        // generamos los elementos para mostrar a partir del carrito
+        carritoSinDuplicados.forEach( (item) => {
+            // obtenemos el elemento que necesitamos para compararlo con la base de datos
+            const miItem = baseDeDatos.filter( (itemBaseDeDatos) => {
+                // verificar si coincide el id en la base de datos
+                return itemBaseDeDatos.id === parseInt(item) // int = numero entero
+            } )
+            // contar el numero de veces que se repite un producto
+            const numeroDeUnidadesItem = carrito.reduce( (total, itemId) => {
+                return itemId === item ? total += 1 : total // esto es como escribir un if
+                // if (itemId === item) {
+                //     return total += 1
+                // } else {
+                //     return total
+                // }
+            }, 0)
+
+            // creamos el nodo del item del carrito
+            const miNodo = document.createElement('li')
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2')
+            miNodo.textContent = `${numeroDeUnidadesItem} x ${miItem[0].nombre} -> ${miItem[0].precio} ${divisa}`
+
+            // aqui vamos a agregar el boton para eliminar cada elemento del carrito
+            // -----------------------
+
+            // agregamos al carrito los nodos
+            DOMcarrito.appendChild(miNodo)
+
+            //creamos una funcion para mostrar el total del carrito
+            // --------------------------------
+
+        } )
+
+        function anadirProductosAlCarrito (evento){
+            // añadimos el nodo a nuestro carrito 
+            carrito.push(evento.target.getAttribute('marcador'))
+            // actualizar carrito
+            renderizarCarrito()
+
+        }
+
     }
 
     renderizarProductos()
